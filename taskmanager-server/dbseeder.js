8db3add6dc8676b69c319233492be3c6b8c469eb
aejmcclelland <aejmcclelland@googlemail.com>
2023-08-20 15:36:58 +0100

    fixed dbseeder.js and added mapbox geocoder

--- taskmanager-server/dbseeder.js
+++ taskmanager-server/dbseeder.js
@@ -4,49 +4,70 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 
 //Load env variables
-dotenv.config();
+dotenv.config({ path: '../taskmanager-server/.env' });
 
-//Load models
-const Task = require('./src/models/tasks');
+// Check if the MONGO_URI is loaded
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
 //connect to DB
-mongoose.connect(process.env.MONGO_URI);
-
-//Read JSOn files
-const tasks = JSON.parse(
-	fs.readFileSync(`${__dirname}/data/tasks.json`, 'utf-8')
-);
-
-tasks.forEach(task => {
-	// Convert the dueDate string to a Date object
-	task.dueDate = new Date(task.dueDate);
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 30000, // Increase this timeout value
 });
 
-//Import into DB
-const importData = async () => {
-	try {
-		await Task.insertMany(tasks);
-		console.log('Data Imported...'.green.inverse);
-		process.exit();
-	} catch (err) {
-		console.error('Error importing data:', err);
-		console.error(err.stack);
-		process.exit(1);
+const db = mongoose.connection;
+
+// Listen for the connection event
+db.on('connected', () => {
+	console.log(`Connected to MongoDB at ${db.host}:${db.port}`.cyan.bold);
+  });
+  
+  // Listen for errors
+  db.on('error', (err) => {
+	console.error(`MongoDB connection error: ${err.message}`.red.bold);
+  });
+
+db.once('open', () => {
+	//Load models
+	const Tasks = require('./src/models/tasks');
+
+	//Read JSOn files
+	const tasks = JSON.parse(
+		fs.readFileSync(`${__dirname}/data/tasks.json`, 'utf-8')
+	);
+
+	tasks.forEach(task => {
+		// Convert the dueDate string to a Date object
+		task.dueDate = new Date(task.dueDate);
+	});
+
+	//Import into DB
+	const importData = async () => {
+		try {
+			await Tasks.insertMany(tasks);
+			console.log('Data Imported...'.green.inverse);
+			process.exit();
+		} catch (err) {
+			console.error('Error importing data:', err);
+			console.error(err.stack);
+			process.exit(1);
+		}
+	};
+	//Delete data from DB
+	const deleteData = async () => {
+		try {
+			await Tasks.deleteMany();
+			console.log('Data Destroyed...'.green.inverse);
+			process.exit();
+		} catch (err) {
+			console.error('Error deleting data:', err);
+			process.exit(err);
+		}
+	};
+	if (process.argv[2] === '-i') {
+		importData();
+	} else if (process.argv[2] === '-d') {
+		deleteData();
 	}
-};
-//Delete data from DB
-const deleteData = async () => {
-	try {
-		await Task.deleteMany();
-		console.log('Data Destroyed...'.green.inverse);
-		process.exit();
-	} catch (err) {
-		console.error('Error deleting data:', err);
-		process.exit(err);
-	}
-};
-if (process.argv[2] === '-i') {
-	importData();
-} else if (process.argv[2] === '-d') {
-	deleteData();
-}
+});
