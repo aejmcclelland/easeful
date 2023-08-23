e343c146dbe2f94a287f8208f963bae380b43fae
aejmcclelland <aejmcclelland@googlemail.com>
2023-08-23 21:55:34 +0100

    added update route and controller

--- taskmanager-server/src/controllers/auth.js
+++ taskmanager-server/src/controllers/auth.js
@@ -57,6 +57,26 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 	});
 });
 
+//@desc     Update user details
+//@route    PUT/api/auth/updatedetails
+//@access   Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+	const fieldsToUpdate = {
+		name: req.body.name,
+		email: req.body.email,
+	};
+
+	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+		new: true,
+		runValidators: true,
+	});
+	
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
+});
+
 //@desc     Forgot password
 //@route    POST /api/auth/forgotpassword
 //@access   Public
